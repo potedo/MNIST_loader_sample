e81c83e21f1efca9313887fe160af3e49493df13
potedo <66190793+potedo@users.noreply.github.com>
2021-02-22 23:32:11 +0900

    Add：layer functions & mnist trainer

---
+++ include/simple_activation.h
@@ -0,0 +1,54 @@
+#ifndef _SIMPLE_ACTIVATION_H_
+#define _SIMPLE_ACTIVATION_H_
+
+#include <algorithm>
+#include <Eigen/Dense>
+
+namespace MyDL{
+
+    using namespace Eigen;
+
+    template<typename T>
+    int step_function(T x){
+        bool y = x > 0;
+        return int(y);
+    }
+
+    template<typename T>
+    double sigmoid(T x){
+        double ret = 1 / (1 + exp(-x));
+        return ret;
+    }
+
+    template<typename T>
+    double relu(T x){
+        double ret = std::max<double>(0, x); 
+        return ret;
+    }
+
+    template<typename T>
+    T identity_function(T x){
+        return x;
+    }
+
+
+    MatrixXd softmax(MatrixXd x){
+        VectorXd max_coeff_vec, rowwise_sum;
+        max_coeff_vec = x.rowwise().maxCoeff();
+
+        x = x.colwise() - max_coeff_vec; // expのオーバーフロー回避 → 行ベクトルごとに最大の要素を抽出
+        x = x.array().exp();             // expを各要素に実行
+        rowwise_sum = x.rowwise().sum(); // 行ベクトルごとに総和を計算
+
+        x.array().colwise() /= rowwise_sum.array(); // 出力の総和が1になるよう調整
+        return x;
+    }
+
+    MatrixXd sigmoid(MatrixXd x){
+        x = x.unaryExpr([] (double p){return 1 / (1 + exp(-p));});
+        return x;
+    }
+
+}
+
+#endif // _SIMPLE_ACTIVATION_H_
